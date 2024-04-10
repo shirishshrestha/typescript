410c10bed8121bf67d13466e02327762ee0f2c8c
SHIRISH <shirish.shrestha07@gmail.com>
2024-04-10 22:56:05 +0545

    learn: varieties of function in typescript

--- typescript_1/src/main.ts
+++ typescript_1/src/main.ts
@@ -1,4 +1,3 @@
-
 let a: number = 12;
 let b: number = 6;
 let c: number = 2;
@@ -7,7 +6,7 @@ console.log(a / b);
 
 console.log(c * b);
 
-function addTwo(num: number) {
+function addTwo(num: number): number {
   return num + 2;
 }
 
@@ -22,4 +21,30 @@ console.log(getUpper("shirish"));
 function signUpUser(name: string, email: string, isPaid: boolean) {}
 console.log(signUpUser("shirish", "shirish@gmai.com", false));
 
-let loginUser = (name: string, email: string, isPaid: boolean = false ) => {};
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
+
+// function getValue(myVal: number) {
+//   if (myVal > 10) {
+//     return myVal;
+//   } else {
+//     return false;
+//   }
+// }
+
+const getHello = (s: string): string => {
+  return "";
+};
+
+const heros = ["thor", "spiderman", "ironman"];
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errorMsg: string): void {
+  console.log(errorMsg);
+}
+
+function handleError(errorMsg: string): never {
+  throw new Error(errorMsg);
+}
