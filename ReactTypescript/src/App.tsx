404204859183cd08e59cee1255294f2d59c914ed
SHIRISH <shirish.shrestha07@gmail.com>
2024-05-01 21:21:31 +0545

    use of generics in react

--- ReactTypescript/src/App.tsx
+++ ReactTypescript/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Counter from "./components/Counter";
 import Heading from "./components/Heading";
 import Section from "./components/Section";
+import List from "./components/List";
 
 const App = () => {
   const [count, setCount] = useState<number>(1);
@@ -11,6 +12,10 @@ const App = () => {
       <Heading title="Hello World" />
       <Section title="Frontend">This is a section</Section>
       <Counter setCount={setCount}>count is {count}</Counter>
+      <List
+        items={["coffee", "tacos", "code"]}
+        render={(item: string) => <span className="gold">{item}</span>}
+      />
     </div>
   );
 };
